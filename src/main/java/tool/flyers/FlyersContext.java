e4a42bcde575bcf4ab520b3503d5d5422bd53185
Sung Bae <laborercode@naver.com>
2016-07-16 18:22:05 +0900

    add some options

--- src/main/java/tool/flyers/FlyersContext.java
+++ src/main/java/tool/flyers/FlyersContext.java
@@ -1,13 +1,22 @@
 package tool.flyers;
 
 public class FlyersContext {
+    private static final String DEFAULT_JADEFILE = "index.jade";
+
     // path of template files
     private String templatePath;
 
     // path of resource files
     private String resourcePath;
 
+    // file name of top level jade
+    private String jadeFileName;
+
     public FlyersContext() {
-        
+        jadeFileName = DEFAULT_JADEFILE;
+    }
+
+    public String getJadeFileName() {
+        return jadeFileName;
     }
 }
--- src/main/java/tool/flyers/OptionParser.java
+++ src/main/java/tool/flyers/OptionParser.java
@@ -2,11 +2,18 @@ package tool.flyers;
 
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 public class OptionParser {
-    private static final String OPTION_ = "";
+    private static final String OPTION_JADE = "j";
+    private static final String OPTION_JADE_LONG = "jade";
+    private static final String OPTION_JADE_DESC = "";
+
+    private static final String OPTION_TEMPLATE_DIR = "t";
+    private static final String OPTION_TEMPLATE_DIR_LONG = "templateDir";
+    private static final String OPTION_TEMPLATE_DIR_DESC = "";
 
     private CommandLineParser parser = new DefaultParser();
     private Options options;
@@ -17,7 +24,21 @@ public class OptionParser {
 
     private Options buildOptions() {
         Options options = new Options();
-        
+
+        Option jadeOption = Option.builder(OPTION_JADE)
+                .longOpt(OPTION_JADE_LONG)
+                .hasArg()
+                .desc(OPTION_JADE_DESC)
+                .build();
+        Option templateDirOption = Option.builder(OPTION_TEMPLATE_DIR)
+                .longOpt(OPTION_TEMPLATE_DIR_LONG)
+                .hasArg()
+                .required()
+                .desc(OPTION_TEMPLATE_DIR_DESC)
+                .build();
+
+        options.addOption(templateDirOption);
+        options.addOption(jadeOption);
         return options;
     }
 
