0f2098eb13b93769046a2f8bea095a14d9f921f4
Sung Bae <laborercode@naver.com>
2016-07-16 18:36:37 +0900

    add help option

--- src/main/java/tool/flyers/OptionParser.java
+++ src/main/java/tool/flyers/OptionParser.java
@@ -1,7 +1,9 @@
 package tool.flyers;
 
+import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
@@ -9,10 +11,12 @@ import org.apache.commons.cli.ParseException;
 public class OptionParser {
     private static final String OPTION_JADE = "j";
     private static final String OPTION_JADE_LONG = "jade";
+    private static final String OPTION_JADE_ARG_NAME = "jadeFileName";
     private static final String OPTION_JADE_DESC = "";
 
     private static final String OPTION_TEMPLATE_DIR = "t";
-    private static final String OPTION_TEMPLATE_DIR_LONG = "templateDir";
+    private static final String OPTION_TEMPLATE_DIR_LONG = "template-dir";
+    private static final String OPTION_TEMPLATE_DIR_ARG_NAME = "templateDir";
     private static final String OPTION_TEMPLATE_DIR_DESC = "";
 
     private CommandLineParser parser = new DefaultParser();
@@ -23,32 +27,46 @@ public class OptionParser {
     }
 
     private Options buildOptions() {
-        Options options = new Options();
+        options = new Options();
 
+        Option helpOption = Option.builder("h")
+                .longOpt("help")
+                .desc("Print help for this application")
+                .build();
         Option jadeOption = Option.builder(OPTION_JADE)
                 .longOpt(OPTION_JADE_LONG)
                 .hasArg()
+                .argName(OPTION_JADE_ARG_NAME)
                 .desc(OPTION_JADE_DESC)
                 .build();
         Option templateDirOption = Option.builder(OPTION_TEMPLATE_DIR)
                 .longOpt(OPTION_TEMPLATE_DIR_LONG)
                 .hasArg()
+                .argName(OPTION_TEMPLATE_DIR_ARG_NAME)
                 .required()
                 .desc(OPTION_TEMPLATE_DIR_DESC)
                 .build();
 
+        options.addOption(helpOption);
         options.addOption(templateDirOption);
         options.addOption(jadeOption);
         return options;
     }
 
     public FlyersContext parse(String[] args) {
+        CommandLine cl;
         FlyersContext context = new FlyersContext();
         try {
-            parser.parse(options, args);
+            cl = parser.parse(options, args);
+
+            if(cl.hasOption('h')) {
+                HelpFormatter helpFormatter = new HelpFormatter();
+                helpFormatter.printHelp("java -jar tool.Flyers", options, true);
+            }
         } catch (ParseException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            HelpFormatter helpFormatter = new HelpFormatter();
+            helpFormatter.printHelp("java -jar tool.Flyers", options, true);
+            System.exit(1);
         }
         return context;
     }
